93f12680b61a47a3b0c83cbf48551bb3fefff4a5
doguebart <douglasweelber.2004@outlook.com>
2024-08-12 16:40:08 -0300

    feat: Create task entity

---
+++ src/domain/tasks/enterprise/tasks.ts
@@ -0,0 +1,57 @@
+import { Entity } from "src/core/entity";
+import { UniqueEntityId } from "src/core/unique-entity-id";
+
+export interface TaskProps {
+  title: string;
+  description: string;
+  isCompleted?: boolean;
+  createdAt: Date;
+  updatedAt?: Date | null;
+}
+
+export class Task extends Entity<TaskProps> {
+  get title() {
+    return this.props.title;
+  }
+
+  set title(title: string) {
+    this.props.title = title;
+    this.touch();
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  set description(description: string) {
+    this.props.description = description;
+    this.touch();
+  }
+
+  get isCompleted() {
+    return this.props.isCompleted;
+  }
+
+  set isCompleted(isCompleted: boolean) {
+    this.props.isCompleted = isCompleted;
+    this.touch();
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(props: TaskProps, id?: UniqueEntityId) {
+    const task = new Task(props, id);
+
+    return task;
+  }
+}
