cdacfe7e7265fb92ca56212845abfc761e53149f
doguebart <douglasweelber.2004@outlook.com>
2024-08-15 22:53:07 -0300

    feat: Create fetchUserTasks & findTaskById methods in tasks repository

--- src/domain/tasks/application/repository/tasks-repository.ts
+++ src/domain/tasks/application/repository/tasks-repository.ts
@@ -1,5 +1,7 @@
 import { Task } from "../../enterprise/tasks";
 
 export abstract class TasksRepository {
+  abstract findById(taskId: string): Promise<Task | void>;
+  abstract fetchUserTasks(userId: string): Promise<Task[]>;
   abstract create(task: Task): Promise<void>;
 }
--- test/repositories/in-memory-tasks-repository.ts
+++ test/repositories/in-memory-tasks-repository.ts
@@ -4,6 +4,24 @@ import { Task } from "@/domain/tasks/enterprise/tasks";
 export class InMemoryTasksRepository implements TasksRepository {
   public items: Task[] = [];
 
+  async findById(taskId: string): Promise<Task | void> {
+    const task = this.items.find((item) => item.id.toString() === taskId);
+
+    if (!task) {
+      return;
+    }
+
+    return task;
+  }
+
+  async fetchUserTasks(userId: string): Promise<Task[]> {
+    const tasks = this.items.filter(
+      (item) => item.userId.toString() === userId
+    );
+
+    return tasks;
+  }
+
   async create(task: Task): Promise<void> {
     this.items.push(task);
   }
